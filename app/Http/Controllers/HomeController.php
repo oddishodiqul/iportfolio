f348a58e81ae2825e17396f07a8f5a8d560e5867
oddishodiqul <oddishodiqul927@gmail.com>
2024-06-12 22:45:26 +0700

    portofolio

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Certificate;
 use App\Models\Education;
 use App\Models\PersonalInfo;
+use App\Models\Portfolio;
 use App\Models\Services;
 use App\Models\Skill;
 use App\Models\SocialMedia;
@@ -21,12 +22,14 @@ class HomeController extends Controller
         $works = WorkExperience::all();
         $educations = Education::all();
         $certificates = Certificate::all();
+        $portfolios = Portfolio::all();
 
         return view('index', [
             'info' => $info,
             'works' => $works,
             'services' => $services,
             'certificates' => $certificates,
+            'portfolios' => $portfolios,
             'skills' => $skills,
             'educations' => $educations,
             'social_media' => $social_media, 
--- database/seeders/PortfolioSeeder.php
+++ database/seeders/PortfolioSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,88 @@ class PortfolioSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('portfolios')->insert([
+            [
+                'title' => 'E-IPO (Electronics Initial Public Offering)',
+                'ownership' => 'Indonesia Stock Exchange',
+                'categories' => 'Web Apps',
+                'descriptions' => 'E-IPO (Electronic Indonesia Public Offering) adalah sistem elektronik berbasis web yang disediakan oleh Bursa Efek Indonesia (BEI) untuk memberikan informasi terkait Penawaran Umum Perdana Saham (IPO) serta untuk melakukan pemesanan saham IPO dari emiten yang sedang melaksanakan Penawaran Umum melalui sistem e-IPO.',
+                'photos' => 'e-ipo.jpg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Manifest (Mandala Intelligence Finance System)',
+                'ownership' => 'PT Mandala Multifinance Tbk',
+                'categories' => 'Web Apps',
+                'descriptions' => 'Manifest adalah sebuah sistem yang digunakan oleh divisi Business Intelligence untuk menganalisa data berdasarkan hasil data report yang bersifat aktual.',
+                'photos' => 'manifest.png',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Web Company Profile',
+                'ownership' => 'KJBNET.id',
+                'categories' => 'Web Company Profile',
+                'descriptions' => 'KJBNET adalah sebuah IT Training Center yang bertujuan untuk menyelenggarakan online maupun offline intensive course yang bergerak di bidang Linux Administration, DevOps dan Web Development.',
+                'photos' => 'kjbnet.jpg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Point of Sales',
+                'ownership' => 'My Cherree Laundry',
+                'categories' => 'Web Apps',
+                'descriptions' => 'Point of Sales (POS) Laundry adalah sebuah sistem yang digunakan sebagai media transaksi pembuatan invoice berdasarkan pada produk laundry.',
+                'photos' => 'mycherree-pos.jpg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Web Company Profile',
+                'ownership' => 'My Cherree Laundry',
+                'categories' => 'Web Company Profile',
+                'descriptions' => 'Point of Sales (POS) Laundry adalah sebuah sistem yang digunakan sebagai media transaksi pembuatan invoice berdasarkan pada produk laundry yang terintegrasi dengan printer thermal.',
+                'photos' => 'mycherree-compro.jpg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Item Tracking / Production Monitoring',
+                'ownership' => 'Jeeves Indonesia',
+                'categories' => 'Web Apps',
+                'descriptions' => 'Item Tracking / Production Monitoring adalah sebuah sistem yang digunakan untuk memantau proses laundry yang dilakukan oleh Jeeves Indonesia yang diidentifikasi menggunakan barcode untuk setiap itemnya.',
+                'photos' => 'jeeves-item-tracking.jpeg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Point of Sales Laundry',
+                'ownership' => 'Jeeves Indonesia',
+                'categories' => 'Web Apps',
+                'descriptions' => 'Point of Sales (POS) Laundry adalah sebuah sistem yang digunakan sebagai media transaksi pembuatan invoice berdasarkan pada produk laundry yang terintegrasi dengan printer thermal.',
+                'photos' => 'jeeves-pos.jpeg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Aplikasi Antrian',
+                'ownership' => 'Jeeves Indonesia',
+                'categories' => 'Web Apps',
+                'descriptions' => 'Aplikasi Antrian adalah sebuah sistem yang digunakan untuk ticketing tamu berdasarkan nomer antrian.',
+                'photos' => 'jeeves-antrian.jpeg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Human Resource Information System (HRIS)',
+                'ownership' => 'Jeeves Indonesia',
+                'categories' => 'Web Apps',
+                'descriptions' => 'Human Resourse Information System (HRIS) adalah sebuah sistem yang digunakan untuk absensi kehadiran, pengajuan cuti, payroll, data karyawan hingga rembruisement.',
+                'photos' => 'jeeves-hris.jpeg',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
     }
 }
